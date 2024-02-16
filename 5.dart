34719eabec2f750142fc50468b84e5416d11339d
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-02-16 17:27:04 +0530

    Update 5.dart

--- 5.dart
+++ 5.dart
@@ -1,156 +1,100 @@
 /*Write a Dart program to find below mentioned operations on the 3x3 array using menu-driven programming approach.
 Perform below all mentioned by user choice:
-• Sum of all elements
-• Sum of specific Row
-• Sum of specific Column
-• Sum of diagonal elements
-• Sum of antidiagonal elements
-• Press 0 for exit
-
-
-[[11, 22, 33], [44, 55, 66], [77, 88, 99]]
-
-1. Sum of all elements
-2. Sum of a specific row
-3. Sum of a specific column
-4. Sum of diagonal elements
-5. Sum of anti-diagonal elements
-0. Exit
-Enter your choice: 1
-Sum of all elements: 495
-
-[[11, 22, 33], [44, 55, 66], [77, 88, 99]]
-
-1. Sum of all elements
-2. Sum of a specific row
-3. Sum of a specific column
-4. Sum of diagonal elements
-5. Sum of anti-diagonal elements
-0. Exit
-Enter your choice: 2
-Enter row number : 2
-Sum of row 2: 165
-
-[[11, 22, 33], [44, 55, 66], [77, 88, 99]]
-
-1. Sum of all elements
-2. Sum of a specific row
-3. Sum of a specific column
-4. Sum of diagonal elements
-5. Sum of anti-diagonal elements
-0. Exit
-Enter your choice: 3
-Enter column number : 2
-Sum of column 2: 165
-
-[[11, 22, 33], [44, 55, 66], [77, 88, 99]]
-
-1. Sum of all elements
-2. Sum of a specific row
-3. Sum of a specific column
-4. Sum of diagonal elements
-5. Sum of anti-diagonal elements
-0. Exit
-Enter your choice: 4
-Sum of diagonal elements: 165
-
-[[11, 22, 33], [44, 55, 66], [77, 88, 99]]
-
-1. Sum of all elements
-2. Sum of a specific row
-3. Sum of a specific column
-4. Sum of diagonal elements
-5. Sum of anti-diagonal elements
-0. Exit
-Enter your choice: 5
-Sum of anti-diagonal elements: 165
-
-[[11, 22, 33], [44, 55, 66], [77, 88, 99]]
-
-1. Sum of all elements
-2. Sum of a specific row
-3. Sum of a specific column
-4. Sum of diagonal elements
-5. Sum of anti-diagonal elements
-0. Exit
-Enter your choice: 0
-[[11, 22, 33], [44, 55, 66], [77, 88, 99]]
 
  */
 import 'dart:io';
 
 void main() {
-  String choice;
-  List<List<int>> arr = [
-    [11, 22, 33],
-    [44, 55, 66],
-    [77, 88, 99]
+  int choice, x, y, sum;
+
+  print("press 1 for sum of all elements..");
+  print("press 2 for rowsum..");
+  print("press 3 for colom sum..");
+  print("press 4 for daigonal..");
+  print("press 5 for anti daigonal..");
+  print("press 6 for exit progaram..");
+
+  stdout.write("Enter your choice :- ");
+  choice = int.parse(stdin.readLineSync()!);
+
+  List a = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
   ];
+  List b = [
+    [5, 5, 5],
+    [5, 5, 5],
+    [5, 5, 5]
+  ];
+  List c = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+  switch (choice) {
+    case 1:
+      for (int i = 0; i <= 2; i++) {
+        for (int j = 0; j <= 2; j++) {
+          c[i][j] = a[i][j] + b[i][j];
+        }
+      }
 
-  do {
-    print("$arr\n");
-    print("1. Sum of all elements");
-    print("2. Sum of a specific row");
-    print("3. Sum of a specific column");
-    print("4. Sum of diagonal elements");
-    print("5. Sum of anti-diagonal elements");
-    print("0. Exit");
-    stdout.write("Enter your choice: ");
-    choice = stdin.readLineSync()!;
-
-    switch (choice) {
-      case "1":
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-          for (int j = 0; j < arr[i].length; j++) {
-            sum = sum + arr[i][j];
+      print("$c");
+      break;
+    case 2:
+      stdout.write("Enter your  row number ");
+      x = int.parse(stdin.readLineSync()!);
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (i == x) {
+            sum = a[x][j] + b[x][j];
+            print("$sum");
           }
         }
-        print("Sum of all elements: $sum\n");
-        break;
+      }
+      break;
 
-      case "2":
-        stdout.write("Enter row number : ");
-        int row = int.parse(stdin.readLineSync()!);
-        int sum = 0;
-        for (int j = 0; j < arr[row - 1].length; j++) {
-          sum = sum + arr[row - 1][j];
+    case 3:
+      stdout.write("Enter your  colom number ");
+      y = int.parse(stdin.readLineSync()!);
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (i == y) {
+            sum = a[i][y] + b[i][y];
+            print("$sum");
+          }
         }
-        print("Sum of row $row: $sum\n");
-        break;
+      }
+      break;
 
-      case "3":
-        stdout.write("Enter column number : ");
-        int col = int.parse(stdin.readLineSync()!);
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-          sum = sum + arr[i][col - 1];
+    case 4:
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (i == j) {
+            sum = a[i][j] + b[i][j];
+            print("$sum");
+          }
         }
-        print("Sum of column $col: $sum\n");
-        break;
+      }
+      break;
 
-      case "4":
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-          sum = sum + arr[i][i];
+    case 5:
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (i + j == a[i][j] - 1) {
+            sum = a[i][j] + b[i][j];
+            print("$sum");
+          }
         }
-        print("Sum of diagonal elements: $sum\n");
-        break;
+      }
+      break;
 
-      case "5":
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-          sum = sum + arr[i][arr.length - i - 1];
-        }
-        print("Sum of anti-diagonal elements: $sum\n");
-        break;
-
-      case "0":
-        break;
-
-      default:
-        print("Invalid choice\n");
-        break;
-    }
-  } while (choice != 0);
+    case 6:
+      stdout.write("data finish......");
+      break;
+    default:
+      stdout.write("invlid input....");
+      break;
+  }
 }
